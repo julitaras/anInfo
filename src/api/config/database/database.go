66cf3dd23bf6ef5973cf472be7c1363407093893
Juli Taras <julibtaras@gmail.com>
2021-11-28 18:52:51 -0300

    ADD gorm to project

--- src/api/config/database/database.go
+++ src/api/config/database/database.go
@@ -3,37 +3,59 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"log"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"proyectos/src/api/config/settings"
 	"sync"
 )
 
 var (
 	once sync.Once
-	db   *sql.DB
+	db   *gorm.DB
 )
 
-//Connection returns a connection to DB
-func Connection() (*sql.DB, error) {
+// Connection returns the connection with database.
+func Connection() (*gorm.DB, error) {
 	var err error
-	log.Println("*Database* Connection start *new instance*")
+
 	var cfg *settings.Data
 	cfg = settings.GetData()
 
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True", cfg.DBConfig.DBUsername, cfg.DBConfig.DBPassword, cfg.DBConfig.DBHost, cfg.DBConfig.DBName)
 
 	once.Do(func() {
-		db, err = sql.Open("mysql", connectionString)
+		var sqlDB *sql.DB
+		sqlDB, err = sql.Open("mysql", connectionString)
+		if err != nil {
+			return
+		}
+
+		db, err = getInstance(sqlDB)
+		if err != nil {
+			return
+		}
 	})
 
 	if err != nil {
-		log.Fatalf("*ERROR* *Database* "+err.Error(), err)
 		return nil, err
 	}
 
-	if errPing := db.Ping(); errPing != nil && cfg.GinMode != "test" {
-		err = errPing
-		log.Fatalf("*ERROR* *Database* Error pinging database: "+errPing.Error(), err)
+	if db == nil {
+		return nil, fmt.Errorf("invalid config")
+	}
+	return db, nil
+}
+
+func getInstance(conn *sql.DB) (*gorm.DB, error) {
+	var err error
+	db, err = gorm.Open(mysql.New(mysql.Config{
+		Conn: conn,
+	}), &gorm.Config{
+		PrepareStmt:            true,
+		SkipDefaultTransaction: true,
+	})
+
+	if err != nil {
 		return nil, err
 	}
 
