44f9524aeb8eb79f05638355a55359eddfb1266b
Alexis Daciuk <adaciuk@gmail.com>
2021-12-12 23:49:16 -0300

    feature/delete task (#5)

--- src/api/task/repository/task_repository.go
+++ src/api/task/repository/task_repository.go
@@ -30,6 +30,16 @@ func (t *TaskRepository) Create(_ context.Context, task *model.Tasks) (*model.Ta
 	return task, nil
 }
 
+func (t *TaskRepository) Delete(_ context.Context, task *model.Tasks) (*model.Tasks, error) {
+	err := t.DB.Delete(&task).Error
+	if err != nil {
+		log.Printf("Error deleting Tasks %v", err)
+		return nil, err
+	}
+
+	return task, nil
+}
+
 func (t *TaskRepository) GetAll(_ context.Context) ([]*model.Tasks, error) {
 	var tasks []*model.Tasks
 
--- src/api/task/service/task_service.go
+++ src/api/task/service/task_service.go
@@ -28,7 +28,16 @@ func (s *TaskService) Insert(ctx context.Context, t *model.Tasks) (*model.Tasks,
 	return r, nil
 }
 
-//Insert service
+func (s *TaskService) Delete(ctx context.Context, t *model.Tasks) (*model.Tasks, error) {
+	r, err := s.r.Delete(ctx, t)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+//GetAll service
 func (s *TaskService) GetAll(ctx context.Context) ([]*model.Tasks, error) {
 	r, err := s.r.GetAll(ctx)
 	if err != nil {
@@ -38,7 +47,7 @@ func (s *TaskService) GetAll(ctx context.Context) ([]*model.Tasks, error) {
 	return r, nil
 }
 
-//Insert service
+//GetById service
 func (s *TaskService) GetById(ctx context.Context, id string) (*model.Tasks, error) {
 	r, err := s.r.GetById(ctx, id)
 	if err != nil {
